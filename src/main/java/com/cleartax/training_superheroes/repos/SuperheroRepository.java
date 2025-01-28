a9ceee9ac6845a0ddfb3b6fc2dc8d9cfd4505b73
Kartik Nagpal <kartik.nagpal@clear.in>
2025-01-29 00:55:41 +0530

    Removed all files from the repository

--- src/main/java/com/cleartax/training_superheroes/repos/SuperheroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cleartax.training_superheroes.repos;
-
-import com.cleartax.training_superheroes.entities.Superhero;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SuperheroRepository extends MongoRepository<Superhero, String> {
-}
